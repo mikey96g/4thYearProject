f1ca3701ce6366c39925043d7098efca4c79ee85
mikey96g <mikey96g@hotmail.com>
2017-12-11 11:44:25 +0000

    Add files via upload

---
+++ Twitter_scraper.py
@@ -0,0 +1,27 @@
+import tweepy
+import csv
+
+####input your credentials here
+consumer_key = 'IXO7y3NtUqgZcki3KuW1p4nS0'
+consumer_secret = 't1MTUtlD1TMemTqpgRyOpzKn3oMIVj3F4Rjoc4mHJsQgUahmtG'
+access_token = '2705490329-80t7yqBWhTvOUAZy3VC4v9SvibbabU2MMsFZuHX'
+access_token_secret = 'u1s6637pHzvF1aHWejgbbjvLIk8esODJVxn4TnhEKqh4k'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+#BitCoin
+#Open/Create a file to append data
+csvFile = open('letsGo.csv', 'a',newline='')
+#Use csv Writer
+fieldnames = ['DateTime','Tweet']
+csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+csvWriter.writeheader()
+
+for tweet in tweepy.Cursor(api.search, q="bitcoin", count=200,
+                           lang="en",
+                           since="2017-12-07").items():
+    if (not tweet.retweeted) and ('RT @' not in tweet.text):
+        print(tweet.created_at, tweet.text)
+        csvWriter.writerow({'DateTime': tweet.created_at, 'Tweet': tweet.text.encode("utf-8")})
+
