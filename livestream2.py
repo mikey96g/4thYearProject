7656e8c918873c5c120840b14dcb24a37e259a3d
mikey96g <mikey96g@hotmail.com>
2018-01-23 03:02:25 +0000

    Add files via upload

---
+++ livestream2.py
@@ -0,0 +1,45 @@
+import csv
+import json
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+#consumer key, consumer secret, access token, access secret.
+ckey='IXO7y3NtUqgZcki3KuW1p4nS0'
+csecret='t1MTUtlD1TMemTqpgRyOpzKn3oMIVj3F4Rjoc4mHJsQgUahmtG'
+atoken='2705490329-80t7yqBWhTvOUAZy3VC4v9SvibbabU2MMsFZuHX'
+asecret='u1s6637pHzvF1aHWejgbbjvLIk8esODJVxn4TnhEKqh4k'
+
+analyzer = SentimentIntensityAnalyzer()
+vs_compound = []
+
+class listener(StreamListener):
+
+
+
+    def on_data(self, data):
+        all_data = json.loads(data)
+        tweet = all_data["text"]
+        if 'RT @' not in tweet:
+            vs = analyzer.polarity_scores(tweet)
+            vs_compound.append(analyzer.polarity_scores(tweet)['compound'])
+            #print("{:-<65} {}".format(tweet, str(vs)))
+
+        print("Sent Score ", vs_compound)
+        return(True)
+
+
+    def on_error(self, status):
+        print(status)
+
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["bitcoin"])
