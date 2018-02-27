26333bea1cee0d565ed67f44ec1373b596f3e494
Michael Gallagher <mikey96g@hotmail.com>
2018-02-27 20:46:16 +0000

    moved file to test

--- test/bitcoin.py
+++ test/bitcoin.py
@@ -1,15 +1,15 @@
 import requests
 
-# req = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-# price = req.json()['bpi']['USD']['rate']
-# time = req.json()['time']['updated']
-# print(price)
-# print(time)
+req = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+price = req.json()['bpi']['USD']['rate']
+time = req.json()['time']['updated']
+print(price)
+print(time)
 # def btc_call():
 #     req = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 #     price = req.json()['bpi']['USD']['rate']
 #     time = req.json()['time']['updated']
 #     print(price)
 #     print(time)
-req = requests.get('https://api.gdax.com/btc/trades')
-print(req.json())
+# req = requests.get('https://api.gdax.com/btc/trades')
+# print(req.json())
