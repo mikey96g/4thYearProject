c90720cfdefa32e768fadc72dcc9b97e5cf62fb7
Michael Gallagher <mikey96g@hotmail.com>
2018-02-28 12:32:10 +0000

    Removed fiels that were not needed and files used when testing

--- test/Main.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-#Open/Create a file to append data
-with open('Data123.csv', 'a', newline='') as csvFile:
-    #Use csv Writer
-    fieldnames = ['ID', 'Tweet']
-    csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
-    csvWriter.writeheader()
