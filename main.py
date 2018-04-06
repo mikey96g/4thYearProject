7da0a9aa93a3f19d98be9e2924431f5241ec9ef5
Michael Gallagher <mikey96g@hotmail.com>
2018-04-06 18:12:18 +0100

    updates to the script

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Output,Event
+from dash.dependencies import Output,Event,Input
 import plotly
 import pyodbc
 import pandas as pd
@@ -10,13 +9,38 @@ import plotly.graph_objs as go
 from collections import deque
 
 
+#Database Access
+cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server}'
+                          ';Server=tcp:year4bitcoin.database.windows.net,1433;'
+                          'Database=year4Proj;Uid=mikey96g@year4bitcoin;Pwd={Tallaght123!};'
+                          'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+cursor = cnxn.cursor()
+# Start the scheduler
+
 X = deque(maxlen=20)
 X.append(1)
 Y = deque(maxlen=20)
 Y.append(1)
 
+sentiment_colors = {-1:"#EE6055",
+                    -0.5:"#FDE74C",
+                     0:"#FFE6AC",
+                     0.5:"#D0F2DF",
+                     1:"#9CEC5B",}
 
 
+app_colors = {
+    'background': '#0C0F0A',
+    'text': '#FFFFFF',
+    'sentiment-plot':'#41EAD4',
+    'volume-bar':'#FBFC74',
+    'someothercolor':'#FF206E',
+}
+def return_prediction():
+    sql = "Select TOP 1 sentTotal from dbo.SentimentValues Order BY  dateS ,timeS  DESC "
+    sq = pd.read_sql(sql, cnxn)
+    sq = sq.values
+    sq[0]
 
 
 app = dash.Dash(__name__)
@@ -27,7 +51,9 @@ app.layout = html.Div(
      html.Div(className='row', children=[html.Div(dcc.Graph(id='live-graph', animate=False), className='col s12 m6 l6'),
                                          html.Div(dcc.Graph(id='historical-graph', animate=False),
                                                   className='col s12 m6 l6')]),
-html.Button('Predict Next Result', id='my-button'),
+     html.Hr(),
+     html.Div([html.Button('Click Me', id='button'),
+     html.H3(id='button-clicks'),]),
 
 
         dcc.Interval(
@@ -38,14 +64,11 @@ html.Button('Predict Next Result', id='my-button'),
 )
 
 
-
 @app.callback(Output('live-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
+
 def update_graph_scatter():
-    cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server}'
-                          ';Server=tcp:year4bitcoin.database.windows.net,1433;'
-                          'Database=year4Proj;Uid=mikey96g@year4bitcoin;Pwd={Tallaght123!};'
-                          'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+
     sql = "Select * from dbo.SentimentValues Order BY  dateS ,timeS  DESC"
     df = pd.read_sql(sql, cnxn)
 
@@ -64,12 +87,10 @@ def update_graph_scatter():
 
 @app.callback(Output('historical-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
+
+
 def update_graph_scatter():
-    cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server}'
-                          ';Server=tcp:year4bitcoin.database.windows.net,1433;'
-                          'Database=year4Proj;Uid=mikey96g@year4bitcoin;Pwd={Tallaght123!};'
-                          'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-    cursor = cnxn.cursor()
+
     sql2 = "Select * from dbo.BitcoinVal Order BY dateB ,dboTime DESC"
     df2 = pd.read_sql(sql2, cnxn)
 
@@ -85,19 +106,25 @@ def update_graph_scatter():
             high=list(high_data),
             low=list(low_data),
             close=list(close_data)
-          
+            #name='Scatter',
+            #mode='lines+markers'
             )
 
     return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
                                                 title="CandleSticky")}
+@app.callback(
+    Output('button-clicks', 'children'),
+    [Input('button', 'n_clicks')])
+def clicks(n_clicks):
+    return 'The predicted value is {} '.format(return_prediction())
+
 
 external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
 for css in external_css:
     app.css.append_css({"external_url": css})
 
-# if __name__ == '__main__':
-#      app.run_server(debug=True)
-server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=https://bitcoinpredict.azurewebsites.net)
+
+server = app.server # the Flask app
+
 if __name__ == '__main__':
-app.run()
+    app.run_server(debug=True)
