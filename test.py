ab7e8577c8feacf2601d9b4e2e692677b5ae409d
mikey96g <mikey96g@hotmail.com>
2018-04-10 18:25:28 +0100

    added scheduler

--- test.py
+++ test.py
@@ -11,6 +11,9 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 import datetime
+from apscheduler.schedulers.background import BlockingScheduler
+import schedule
+import time
 
 # fix random seed for reproducibility
 np.random.seed(7)
@@ -27,122 +30,116 @@ def create_dataset(dataset, look_back=1):
 #Database Access
 cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server}'
                       ';Server=tcp:year4bitcoin.database.windows.net,1433;'
-                      'Database=year4Proj;Uid=mikey96g@year4bitcoin;Pwd={};'
+                      'Database=year4Proj;Uid=mikey96g@year4bitcoin;Pwd={Tallaght123!};'
                       'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
 
 
-print(datetime.datetime.now().time())
+
 look_back = 1
-					  
+#sched = BlockingScheduler()					  
+def run_model():
+	sql = "Select * from dbo.BitcoinVal"
+	df = pd.read_sql(sql,cnxn)
+		
+	df_stock = df.copy()
+	df_stock.drop(['dboTime'],1,inplace=True)
+	df_stock.drop(['timeDate'],1,inplace=True)
+	df_stock.drop(['dateTimeS'],1,inplace=True)
+	df_stock.drop(['dateB'],1,inplace=True)
+	df_stock.drop(['bandUpper'],1,inplace=True)
+	df_stock.drop(['bandLower'],1,inplace=True)
 
-sql = "Select * from dbo.BitcoinVal"
-df = pd.read_sql(sql,cnxn)
-	
-df_stock = df.copy()
-df_stock.drop(['dboTime'],1,inplace=True)
-df_stock.drop(['timeDate'],1,inplace=True)
-df_stock.drop(['dateTimeS'],1,inplace=True)
-df_stock.drop(['dateB'],1,inplace=True)
-df_stock.drop(['bandUpper'],1,inplace=True)
-df_stock.drop(['bandLower'],1,inplace=True)
+	# normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset = scaler.fit_transform(df_stock)
+	train_size = int(len(dataset) * 0.8)
+	test_size = len(dataset) - train_size
+	train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+	print(len(train), len(test))
 
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(df_stock)
-train_size = int(len(dataset) * 0.8)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
-
-# reshape into X=t and Y=t+1
-look_back = 1
-trainX, trainY = create_dataset(train, look_back)
-testX, testY = create_dataset(test, look_back)
-	
-	# reshape input to be [samples, time steps, features]
-trainX = np.reshape(trainX, (trainX.shape[0], look_back, 6))
-testX = np.reshape(testX, (testX.shape[0], look_back, 6))
-	
-#	return trainX ,trainY,testX, testY
-
-
-#def create model():	
-	# create and fit the LSTM network
-model = Sequential()
-model.add(LSTM(4, input_shape=(look_back,6)))
-model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-#	return model
-
-#trainX ,trainY,testX, testY= load_data()	
-	
-history= model.fit(trainX, trainY,validation_split=0.33, nb_epoch=100, batch_size=10)
+	# reshape into X=t and Y=t+1
+	look_back = 1
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
+		
+	#reshape input to be [samples, time steps, features]
+	trainX = np.reshape(trainX, (trainX.shape[0], look_back, 6))
+	testX = np.reshape(testX, (testX.shape[0], look_back, 6))
+		
 
 
 
+	#def create model():	
+	#create and fit the LSTM network
+	model = Sequential()
+	model.add(LSTM(4, input_shape=(look_back,6)))
+	model.add(Dense(1))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	#	return model
 
-#make predictions
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-
-# Get something which has as many features as dataset
-trainPredict_extended = np.zeros((len(trainPredict),6))
-# Put the predictions there
-trainPredict_extended[:,5] = trainPredict[:,0]
-# Inverse transform it and select the 3rd column.
-trainPredict = scaler.inverse_transform(trainPredict_extended) [:,1]  
-
-# Get something which has as many features as dataset
-testPredict_extended = np.zeros((len(testPredict),6))
-# Put the predictions there
-testPredict_extended[:,5] = testPredict[:,0]
-# Inverse transform it and select the 2nd column.
-testPredict = scaler.inverse_transform(testPredict_extended) [:,1]  
+	#trainX ,trainY,testX, testY= load_data()	
+		
+	history= model.fit(trainX, trainY,validation_split=0.33, nb_epoch=100, batch_size=10)
 
 
-trainY_extended = np.zeros((len(trainY),6))
-trainY_extended[:,5]=trainY
-trainY=scaler.inverse_transform(trainY_extended)[:,1]
 
 
-testY_extended = np.zeros((len(testY),6))
-testY_extended[:,5]=testY
-testY=scaler.inverse_transform(testY_extended)[:,1]
+	#make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+
+	# Get something which has as many features as dataset
+	trainPredict_extended = np.zeros((len(trainPredict),6))
+	# Put the predictions there
+	trainPredict_extended[:,5] = trainPredict[:,0]
+	# Inverse transform it and select the 3rd column.
+	trainPredict = scaler.inverse_transform(trainPredict_extended) [:,1]  
+
+	# Get something which has as many features as dataset
+	testPredict_extended = np.zeros((len(testPredict),6))
+	# Put the predictions there
+	testPredict_extended[:,5] = testPredict[:,0]
+	# Inverse transform it and select the 2nd column.
+	testPredict = scaler.inverse_transform(testPredict_extended) [:,1]  
 
 
-multiN = testY[0]
-sentN = 5
-date = datetime.datetime.now().date()
-time = datetime.datetime.now().time()
-ds =timeDate = datetime.datetime.now()
-
-cursor.execute("INSERT INTO dbo.results "
-                   "(lstmSent,lstmMulti,rTime,rDate,timeDate)"
-                   " values(?,?,?,?,?)", sentN,multiN,time, date,ds)
-cnxn.commit() 
-
-# shift train predictions for plotting
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, 5] = trainPredict
+	trainY_extended = np.zeros((len(trainY),6))
+	trainY_extended[:,5]=trainY
+	trainY=scaler.inverse_transform(trainY_extended)[:,1]
 
 
-# shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, 5] = testPredict
+	testY_extended = np.zeros((len(testY),6))
+	testY_extended[:,5]=testY
+	testY=scaler.inverse_transform(testY_extended)[:,1]
 
-print(testPredictPlot)
-#plot
-original,=plt.plot(scaler.inverse_transform(dataset)[:,1])  
-training_pred,=plt.plot(trainPredictPlot[:,1],linestyle='--')  
-test_pred,=plt.plot(testPredictPlot[:,1],linestyle='--')
-plt.title('Bitcoin Price')
-plt.ylabel('price')
-plt.xlabel('intervals')
-plt.legend([original,training_pred,test_pred],['original','training_pred','test'], loc='upper right')
-plt.show()
+
+	multiN = testY[0]
+	sentN = 5
+	date = datetime.datetime.now().date()
+	time = datetime.datetime.now().time()
+	ds =timeDate = datetime.datetime.now()
+
+	cursor.execute("INSERT INTO dbo.results "
+					   "(lstmSent,lstmMulti,rTime,rDate,timeDate)"
+					   " values(?,?,?,?,?)", sentN,multiN,time, date,ds)
+	cnxn.commit() 
+	return  print('Committeded')
+
+
+
+schedule.every(1).minutes.do(run_model)
+#sched.add_job(run_model, 'interval', seconds=60)
+#sched.start()
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+cnxn.close()
+
 
 
 
